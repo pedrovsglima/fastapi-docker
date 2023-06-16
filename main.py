86966449058572937ebf1c87cea6efbf459cb786
peuvitor <p.vitor31@hotmail.com>
2023-06-16 11:35:45 -0300

    test get team from json file

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
-from http.client import HTTPException
+import json
 import pandas as pd
+from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
+from http.client import HTTPException
 
 app = FastAPI()
 
+# def csv_to_dict(file:str, cols:List[str]=None) -> dict:
+#     df = pd.read_csv(
+#             file,
+#             delimiter=",",
+#             usecols=cols,
+#         ).drop_duplicates(inplace=True)
+    
+#     return df.to_dict("records")
+
+@app.get("/")
+def index() -> dict:
+    return {"status": "OK"}
+
 @app.get("/teams/{team_abb}")
 def query_team_by_abbreviation(team_abb:str) -> str:
-    teams = pd.read_csv(
-        "./nba-stats/team-summaries.csv",
-        delimiter=",",
-        usecols=["team", "abbreviation"]
-    ).drop_duplicates(inplace=True)
+    
+    teams = json.load(open("teams.json"))
+    
+    for _,v in enumerate(teams):
+        abb = v.get("abbreviation", "")
+        if abb == team_abb:
+            return v.get("team", "")
+        
+    return f"No team has the abbreviation: {team_abb}"    
 
-    team = teams[teams["abbreviation"]==team_abb]
+    # teams = csv_to_dict(
+    #     file="./nba-stats/team-summaries.csv", 
+    #     cols=["team", "abbreviation"]
+    # )
+
+    # team = teams[teams["abbreviation"]==team_abb]
     
-    if team.empty:
-        raise HTTPException(
-            status_code=404,
-            detail=f"No team has the abbreviation: {team_abb}"
-        )
+    # if team.empty:
+    #     raise HTTPException(
+    #         status_code=404,
+    #         detail=f"No team has the abbreviation: {team_abb}"
+    #     )
     
-    return team["team"]
+    # return team["team"]
--- test_server.py
+++ test_server.py
@@ -1,8 +1,10 @@
 import requests
 
-URL_BASE = "http://127.0.0.1:8000"
+URL_BASE = "http://127.0.0.1:8000/"
+# endpoint = ""
+endpoint = "teams/LAL"
 
-response = requests.get(f"{URL_BASE}/teams/ATL")
+response = requests.get(URL_BASE+endpoint)
 
 if response.status_code==200:
     print(response.json())
