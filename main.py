01f1fb30755f06313d87717d197b641c3baa31a5
peuvitor <p.vitor31@hotmail.com>
2023-06-20 12:24:24 -0300

    add query_team_by_parameters

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+with open("./nba-stats/team-summaries.csv","r") as f:
+    teams = list(DictReader(f))
+
+
 @app.get("/")
 def index() -> dict:
     return {"status": "OK"}
 
+
 @app.get("/teams/{team_abb}")
 def query_team_by_abbreviation(team_abb:str) -> str:
-    
-    with open("./nba-stats/team-summaries.csv","r") as f:
-        teams = list(DictReader(f))
     
     for _,v in enumerate(teams):
         abb = v.get("abbreviation", "")
@@ -20,3 +23,20 @@ def query_team_by_abbreviation(team_abb:str) -> str:
             return v.get("team", "")
         
     return f"No team has the following abbreviation: {team_abb}"
+
+
+@app.get("/teams/")
+def query_team_by_parameters(abb:str, season:str) -> dict:
+
+    for _,v in enumerate(teams):
+        current_abb = v.get("abbreviation", "")
+        current_season = v.get("season", "")
+        if current_abb == abb and current_season == season:
+            return {
+                "abbreviation": v.get("abbreviation", ""),
+                "team": v.get("team", ""),
+                "season": v.get("season", ""),
+                "age": v.get("age", ""),
+            }
+
+    return {"message": f"No team from season {season} has the abbreviation {abb}"}
--- test_server.py
+++ test_server.py
@@ -2,9 +2,14 @@ import requests
 
 URL_BASE = "http://127.0.0.1:8000/"
 # endpoint = ""
-endpoint = "teams/LAL"
+# endpoint = "teams/LAL"
+endpoint = "teams"
+# params = ""
+params = "?abb=LAL&season=2023"
 
-response = requests.get(URL_BASE+endpoint)
+url = URL_BASE+endpoint+params
+print(url)
+response = requests.get(url)
 
 if response.status_code==200:
     print(response.json())
