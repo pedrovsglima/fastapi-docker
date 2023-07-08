08bd1cbbdb0a6c3066bcd70d670bcb43e9b5505a
peuvitor <p.vitor31@hotmail.com>
2023-07-08 11:07:41 -0300

    add all tables to postgres

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -1,36 +1,211 @@
 CREATE SCHEMA team;
+CREATE SCHEMA player;
 
-DROP TABLE IF EXISTS team.summaries;
+CREATE TABLE team.end_of_season (
+    season          INT               NOT NULL
+    ,lg             VARCHAR(255)      NULL
+    ,type           VARCHAR(255)      NULL
+    ,number_tm      VARCHAR(255)      NULL
+    ,player         VARCHAR(255)      NOT NULL
+    ,position       VARCHAR(255)      NULL
+    ,seas_id        INT               NULL
+    ,player_id      INT               NULL
+    ,birth_year     INT               NULL
+    ,tm             VARCHAR(255)      NOT NULL
+    ,age            INT               NULL
+);
+
 CREATE TABLE team.summaries (
-    season            int                   NOT NULL	
-    ,lg                varchar(255)         NULL
-    ,team              varchar(255)         NOT NULL
-    ,abbreviation      varchar(255)         NULL
-    ,playoffs          bool                 NULL
-    ,age               float                NULL
-    ,w                 int                  NULL
-    ,l                 int                  NULL
-    ,pw                int                  NULL
-    ,pl                int                  NULL
-    ,mov               float                NULL
-    ,sos               float                NULL
-    ,srs               float                NULL
-    ,o_rtg             float                NULL
-    ,d_rtg             float                NULL
-    ,n_rtg             float                NULL
-    ,pace              float                NULL
-    ,f_tr              float                NULL
-    ,x3p_ar            float                NULL
-    ,ts_percent        float                NULL
-    ,e_fg_percent      float                NULL
-    ,tov_percent       float                NULL
-    ,orb_percent       float                NULL
-    ,ft_fga            float                NULL
-    ,opp_e_fg_percent  float                NULL
-    ,opp_tov_percent   float                NULL
-    ,opp_drb_percent   float                NULL
-    ,opp_ft_fga        float                NULL
-    ,arena             varchar(255)         NULL
-    ,attend            int                  NULL	
-    ,attend_g          int                  NULL    
+    season             INT                  NOT NULL	
+    ,lg                VARCHAR(255)         NULL
+    ,team              VARCHAR(255)         NOT NULL
+    ,abbreviation      VARCHAR(255)         NULL
+    ,playoffs          BOOL                 NULL
+    ,age               FLOAT                NULL
+    ,w                 INT                  NULL
+    ,l                 INT                  NULL
+    ,pw                INT                  NULL
+    ,pl                INT                  NULL
+    ,mov               FLOAT                NULL
+    ,sos               FLOAT                NULL
+    ,srs               FLOAT                NULL
+    ,o_rtg             FLOAT                NULL
+    ,d_rtg             FLOAT                NULL
+    ,n_rtg             FLOAT                NULL
+    ,pace              FLOAT                NULL
+    ,f_tr              FLOAT                NULL
+    ,x3p_ar            FLOAT                NULL
+    ,ts_percent        FLOAT                NULL
+    ,e_fg_percent      FLOAT                NULL
+    ,tov_percent       FLOAT                NULL
+    ,orb_percent       FLOAT                NULL
+    ,ft_fga            FLOAT                NULL
+    ,opp_e_fg_percent  FLOAT                NULL
+    ,opp_tov_percent   FLOAT                NULL
+    ,opp_drb_percent   FLOAT                NULL
+    ,opp_ft_fga        FLOAT                NULL
+    ,arena             VARCHAR(255)         NULL
+    ,attend            INT                  NULL	
+    ,attend_g          INT                  NULL    
+);
+
+CREATE TABLE team.stats_per_game (
+    season                 INT               NOT NULL	
+    ,lg                    VARCHAR(255)      NULL	
+    ,team                  VARCHAR(255)      NOT NULL	
+    ,abbreviation          VARCHAR(255)      NULL	
+    ,playoffs              BOOL              NULL	
+    ,g                     INT               NULL	
+    ,mp_per_game           FLOAT             NULL	
+    ,fg_per_game           FLOAT             NULL	
+    ,fga_per_game          FLOAT             NULL	
+    ,fg_percent            FLOAT             NULL	
+    ,x3p_per_game          FLOAT             NULL	
+    ,x3pa_per_game         FLOAT             NULL	
+    ,x3p_percent           FLOAT             NULL	
+    ,x2p_per_game          FLOAT             NULL	
+    ,x2pa_per_game         FLOAT             NULL	
+    ,x2p_percent           FLOAT             NULL	
+    ,ft_per_game           FLOAT             NULL	
+    ,fta_per_game          FLOAT             NULL	
+    ,ft_percent            FLOAT             NULL	
+    ,orb_per_game          FLOAT             NULL	
+    ,drb_per_game          FLOAT             NULL	
+    ,trb_per_game          FLOAT             NULL	
+    ,ast_per_game          FLOAT             NULL	
+    ,stl_per_game          FLOAT             NULL	
+    ,blk_per_game          FLOAT             NULL	
+    ,tov_per_game          FLOAT             NULL	
+    ,pf_per_game           FLOAT             NULL	
+    ,pts_per_game          FLOAT             NULL
+);
+
+CREATE TABLE team.totals (
+    season                 INT               NOT NULL	
+    ,lg                    VARCHAR(255)      NULL	
+    ,team                  VARCHAR(255)      NOT NULL	
+    ,abbreviation          VARCHAR(255)      NULL	
+    ,playoffs              BOOL              NULL	
+    ,g                     INT               NULL
+    ,mp                    INT               NULL	
+    ,fg                    INT               NULL	
+    ,fga                   INT               NULL	
+    ,fg_percent            FLOAT             NULL	
+    ,x3p                   INT               NULL	
+    ,x3pa                  INT               NULL	
+    ,x3p_percent           FLOAT             NULL	
+    ,x2p                   INT               NULL	
+    ,x2pa                  INT               NULL	
+    ,x2p_percent           FLOAT             NULL	
+    ,ft                    INT               NULL	
+    ,fta                   INT               NULL	
+    ,ft_percent            FLOAT             NULL	
+    ,orb                   INT               NULL	
+    ,drb                   INT               NULL	
+    ,trb                   INT               NULL	
+    ,ast                   INT               NULL	
+    ,stl                   INT               NULL	
+    ,blk                   INT               NULL	
+    ,tov                   INT               NULL	
+    ,pf                    INT               NULL	
+    ,pts                   INT               NULL
+);
+
+CREATE TABLE player.career_info (
+    player_id        INT            NOT NULL	
+    ,player          VARCHAR(255)   NULL	
+    ,birth_year      INT            NULL	
+    ,hof             BOOL           NULL	
+    ,num_seasons     INT            NULL	
+    ,first_seas      INT            NULL	
+    ,last_seas       INT            NULL
+);
+
+CREATE TABLE player.season_info (
+    season          INT               NULL
+    ,seas_id        INT               NOT NULL
+    ,player_id      INT               NOT NULL
+    ,player         VARCHAR(255)      NULL
+    ,birth_year     INT               NULL
+    ,pos            VARCHAR(255)      NULL
+    ,age            INT               NULL
+    ,lg             VARCHAR(255)      NULL
+    ,tm             VARCHAR(255)      NULL
+    ,experience     INT               NULL 
+);
+
+CREATE TABLE player.stats_per_game (
+    seas_id               INT            NOT NULL	
+    ,season               INT            NULL	
+    ,player_id            INT            NOT NULL	
+    ,player               VARCHAR(255)   NULL	
+    ,birth_year           INT            NULL	
+    ,pos                  VARCHAR(255)   NULL	
+    ,age                  INT            NULL	
+    ,experience           INT            NULL	
+    ,lg                   VARCHAR(255)   NULL	
+    ,tm                   VARCHAR(255)   NULL	
+    ,g                    INT            NULL	
+    ,gs                   INT            NULL	
+    ,mp_per_game          FLOAT          NULL	
+    ,fg_per_game          FLOAT          NULL	
+    ,fga_per_game         FLOAT          NULL	
+    ,fg_percent           FLOAT          NULL	
+    ,x3p_per_game         FLOAT          NULL	
+    ,x3pa_per_game        FLOAT          NULL	
+    ,x3p_percent          FLOAT          NULL	
+    ,x2p_per_game         FLOAT          NULL	
+    ,x2pa_per_game        FLOAT          NULL	
+    ,x2p_percent          FLOAT          NULL	
+    ,e_fg_percent         FLOAT          NULL	
+    ,ft_per_game          FLOAT          NULL	
+    ,fta_per_game         FLOAT          NULL	
+    ,ft_percent           FLOAT          NULL	
+    ,orb_per_game         FLOAT          NULL	
+    ,drb_per_game         FLOAT          NULL	
+    ,trb_per_game         FLOAT          NULL	
+    ,ast_per_game         FLOAT          NULL	
+    ,stl_per_game         FLOAT          NULL	
+    ,blk_per_game         FLOAT          NULL	
+    ,tov_per_game         FLOAT          NULL	
+    ,pf_per_game          FLOAT          NULL	
+    ,pts_per_game         FLOAT          NULL
+);
+
+CREATE TABLE player.totals (
+    seas_id             INT                 NOT NULL	
+    ,season             INT                 NULL	
+    ,player_id          INT                 NOT NULL	
+    ,player             VARCHAR(255)        NULL	
+    ,birth_year         INT                 NULL	
+    ,pos                VARCHAR(255)        NULL	
+    ,age                INT                 NULL	
+    ,experience         INT                 NULL	
+    ,lg                 VARCHAR(255)        NULL	
+    ,tm                 VARCHAR(255)        NULL	
+    ,g                  INT                 NULL	
+    ,gs                 INT                 NULL	
+    ,mp                 INT                 NULL	
+    ,fg                 INT                 NULL	
+    ,fga                INT                 NULL	
+    ,fg_percent         FLOAT               NULL	
+    ,x3p                INT                 NULL	
+    ,x3pa               INT                 NULL	
+    ,x3p_percent        FLOAT               NULL	
+    ,x2p                INT                 NULL	
+    ,x2pa               INT                 NULL	
+    ,x2p_percent        FLOAT               NULL	
+    ,e_fg_percent       FLOAT               NULL	
+    ,ft                 INT                 NULL	
+    ,fta                INT                 NULL	
+    ,ft_percent         FLOAT               NULL	
+    ,orb                INT                 NULL	
+    ,drb                INT                 NULL	
+    ,trb                INT                 NULL	
+    ,ast                INT                 NULL	
+    ,stl                INT                 NULL	
+    ,blk                INT                 NULL	
+    ,tov                INT                 NULL	
+    ,pf                 INT                 NULL	
+    ,pts                INT                 NULL
 );
--- sql-scripts/fill_tables.sql
+++ sql-scripts/fill_tables.sql
@@ -1,5 +1,47 @@
+COPY team.end_of_season (season, lg, type, number_tm, player, position, seas_id, player_id, birth_year, tm, age)
+FROM '/nba-stats/end-of-season-teams.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
 COPY team.summaries (season, lg, team, abbreviation, playoffs, age, w, l, pw, pl, mov, sos, srs, o_rtg, d_rtg, n_rtg, pace, f_tr, x3p_ar, ts_percent, e_fg_percent, tov_percent, orb_percent, ft_fga, opp_e_fg_percent, opp_tov_percent, opp_drb_percent, opp_ft_fga, arena, attend, attend_g)
 FROM '/nba-stats/team-summaries.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY team.stats_per_game (season, lg, team, abbreviation, playoffs, g, mp_per_game, fg_per_game, fga_per_game, fg_percent, x3p_per_game, x3pa_per_game, x3p_percent, x2p_per_game, x2pa_per_game, x2p_percent, ft_per_game, fta_per_game, ft_percent, orb_per_game, drb_per_game, trb_per_game, ast_per_game, stl_per_game, blk_per_game, tov_per_game, pf_per_game, pts_per_game)
+FROM '/nba-stats/team-stats-per-game.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY team.totals (season, lg, team, abbreviation, playoffs, g, mp, fg, fga, fg_percent, x3p, x3pa, x3p_percent, x2p, x2pa, x2p_percent, ft, fta, ft_percent, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+FROM '/nba-stats/team-totals.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY player.career_info (player_id, player, birth_year, hof, num_seasons, first_seas, last_seas)
+FROM '/nba-stats/player-career-info.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY player.season_info (season, seas_id, player_id, player, birth_year, pos, age, lg, tm, experience)
+FROM '/nba-stats/player-season-info.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY player.stats_per_game (seas_id, season, player_id, player, birth_year, pos, age, experience, lg, tm, g, gs, mp_per_game, fg_per_game, fga_per_game, fg_percent, x3p_per_game, x3pa_per_game, x3p_percent, x2p_per_game, x2pa_per_game, x2p_percent, e_fg_percent, ft_per_game, fta_per_game, ft_percent, orb_per_game, drb_per_game, trb_per_game, ast_per_game, stl_per_game, blk_per_game, tov_per_game, pf_per_game, pts_per_game)
+FROM '/nba-stats/player-per-game.csv'
+DELIMITER ','
+NULL AS 'NA'
+CSV HEADER;
+
+COPY player.totals (seas_id, season, player_id, player, birth_year, pos, age, experience, lg, tm, g, gs, mp, fg, fga, fg_percent, x3p, x3pa, x3p_percent, x2p, x2pa, x2p_percent, e_fg_percent, ft, fta, ft_percent, orb, drb, trb, ast, stl, blk, tov, pf, pts)
+FROM '/nba-stats/player-totals.csv'
 DELIMITER ','
 NULL AS 'NA'
 CSV HEADER;
